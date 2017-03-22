015d489ca9ba0c43d71589ba84c5e45e48708e1a
jspwoody <jspwoody@qq.com>
2017-03-22 19:40:52 +0800

    修改代码

---
+++ src/main/java/com/bkm/spring/controller/StringToPhoneNumberConverter.java
@@ -0,0 +1,33 @@
+package com.bkm.spring.controller;
+
+import com.bkm.spring.model.PhoneNumberModel;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.util.StringUtils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//String-->PhoneNumberModel
+public class StringToPhoneNumberConverter implements Converter<String, PhoneNumberModel>
+{
+    Pattern pattern = Pattern.compile("^(\\d{3,4})-(\\d{7,8})$");
+    public PhoneNumberModel convert(String source){
+
+        if (!StringUtils.hasLength(source)){
+            return null;
+        }
+
+        Matcher matcher = pattern.matcher(source);
+        if (matcher.matches()){
+            PhoneNumberModel phoneNumber = new PhoneNumberModel();
+            phoneNumber.setAreaCode(matcher.group(1));
+            phoneNumber.setPhoneNumber(matcher.group(2));
+            return phoneNumber;
+        }
+        else{
+            throw new IllegalArgumentException(String.format("类型转换失败，需要格式[010-12345678]，但格式是[%s]", source));
+        }
+    }
+}
+
+
