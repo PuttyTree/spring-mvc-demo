ef085690b5a8b04a66c9202e569e1479e775d639
jspwoody <jspwoody@qq.com>
2017-07-07 18:51:03 +0800

    --

--- spring-mvc-app/src/main/java/com/bkm/spring/model/User.java
+++ spring-mvc-app/src/main/java/com/bkm/spring/model/User.java
@@ -2,88 +2,104 @@ package com.bkm.spring.model;
 
 import java.util.Date;
 
-public class User
-{
+public class User {
 
-    private int id = -1;
-    private String name;
-    private String phone;
-    private String email;
-    private String pwd;
-    private String pwdSalt;
-    private Date createTime;
-    private Date modifyTime;
-    private Short isDelete;
+	private int id = -1;
+	private String name;
+	private String phone;
+	private String email;
+	private String pwd;
+	private String pwdSalt;
+	private Date createTime;
+	private Date modifyTime;
+	private Short delete;
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getPhone() {
-        return phone;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getPwd() {
-        return pwd;
-    }
+	public String getPwd() {
+		return pwd;
+	}
 
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
-    }
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
 
-    public String getPwdSalt() {
-        return pwdSalt;
-    }
+	public String getPwdSalt() {
+		return pwdSalt;
+	}
 
-    public void setPwdSalt(String pwdSalt) {
-        this.pwdSalt = pwdSalt;
-    }
+	public void setPwdSalt(String pwdSalt) {
+		this.pwdSalt = pwdSalt;
+	}
 
-    public Date getCreateTime() {
-        return createTime;
-    }
+	public Date getCreateTime() {
+		return createTime;
+	}
 
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
 
-    public Date getModifyTime() {
-        return modifyTime;
-    }
+	public Date getModifyTime() {
+		return modifyTime;
+	}
 
-    public void setModifyTime(Date modifyTime) {
-        this.modifyTime = modifyTime;
-    }
+	public void setModifyTime(Date modifyTime) {
+		this.modifyTime = modifyTime;
+	}
 
-    public Short getIsDelete() {
-        return isDelete;
-    }
+	public Short getDelete() {
+		return delete;
+	}
 
-    public void setIsDelete(Short isDelete) {
-        this.isDelete = isDelete;
-    }
+	public void setDelete(Short delete) {
+		this.delete = delete;
+	}
+
+	@Override
+	public String toString() {
+
+		final StringBuilder sb = new StringBuilder("User{");
+		sb.append("id=").append(id);
+		sb.append(",phone=").append(phone);
+		sb.append(",email=").append(email);
+		sb.append(",pwd=").append(pwd);
+		sb.append(",pwdSalt=").append(pwdSalt);
+		sb.append(",createTime=").append(createTime);
+		sb.append(",modifyTime=").append(modifyTime);
+		sb.append(",delete=").append(delete);
+		sb.append("}");
+
+		return sb.toString();
+	}
 }
